360af2c95a26d3195888135aabfb6641202c6c9a
Khaidar Bikmaev <khaidar@bikmaev.com>
2023-09-19 13:59:42 +0000

    feat(asdf): finally works!!!!!

--- extract.py
+++ extract.py
@@ -3,7 +3,10 @@ from google.protobuf import text_format
 from google.protobuf import json_format
 from pathlib import Path
 from typing import List, Dict
-from json import loads
+from json import loads, dump
+from time import sleep
+import requests
+from tqdm import tqdm
 
 
 font = str
@@ -62,13 +65,90 @@ def extract_fonts(metadata: List[str]):
     return fonts
 
 
-def export_to_fonts_js(fonts: Fonts):
-    with open("fonts.js", "w") as f:
-        f.write("module.exports = ")
-        f.write(str(fonts))
+def save_to_json(json, file_name="fonts.json"):
+    with open(file_name, "w") as json_file:
+        dump(
+            json,
+            json_file,
+            indent=None,
+            separators=(",", ":"),
+        )
+
+
+def class_name(font_name: str, font_style: str, font_weight: int):
+    font_name = font_name.replace(" ", "")
+    if font_style == "italic":
+        font_style = "i"
+    if font_style == "normal":
+        font_style = ""
+    clazz = f".font-{font_name}-{font_weight}"
+    if font_style == "i":
+        return f"{clazz}-i"
+    return clazz
+
+
+def create_utils(fonts: Fonts):
+    utils = {}
+    for font_name in fonts.keys():
+        variants = fonts[font_name]
+        for font_style in variants.keys():
+            variant = variants[font_style]
+            for font_weight, exact_name in variant.items():
+                utils[class_name(font_name, font_style, font_weight)] = {
+                    "font-family": exact_name,
+                    "font-weight": font_weight,
+                    "font-style": "italic" if font_style == "i" else "normal",
+                }
+    return utils
+
+
+def font_face_url(font_name: str, font_style: str, font_weight: int):
+    url = f"https://fonts.googleapis.com/css2?family={font_name.replace(' ', '+')}"
+    if font_style == "i":
+        url += ":ital,wght@"
+        url += f"1,{font_weight}"
+    else:
+        url += ":wght@"
+        url += f"{font_weight}"
+    return url
+
+
+def create_font_faces(fonts: Fonts, utils: dict):
+    font_faces = {}
+    with tqdm(total=len(utils.keys())) as pbar:
+        for font_name in fonts.keys():
+            variants = fonts[font_name]
+            for font_style in variants.keys():
+                variant = variants[font_style]
+                for font_weight, exact_name in variant.items():
+                    sleep(0.05)
+                    clazz = class_name(font_name, font_style, font_weight)
+                    url = font_face_url(exact_name, font_style, font_weight)
+                    req = requests.get(url)
+                    if req.status_code != 200:
+                        print(f"{url=}")
+                        print(f"{req.status_code=}")
+                        pbar.update(1)
+                        continue
+                    font_faces[clazz] = req.text
+                    pbar.update(1)
+    return font_faces
+
+
+def delete_missing(font_faces: Dict[str, str], utils: Dict[str, str]):
+    util_keys = list(utils.keys())
+    for clazz in util_keys:
+        if clazz not in font_faces.keys():
+            del utils[clazz]
+    return utils
 
 
 if __name__ == "__main__":
     jsons = parse_protobufs()
-    export_to_fonts_js(extract_fonts(jsons))
+    fonts = extract_fonts(jsons)
     print(f"{extract_errors=}")
+    utils = create_utils(fonts)
+    font_faces = create_font_faces(fonts, utils)
+    save_to_json(font_faces, "fonts.json")
+    utils = delete_missing(font_faces, utils)
+    save_to_json(utils, "utils.json")
--- index.js
+++ index.js
@@ -1,14 +1,72 @@
-let plugin = require("tailwindcss/plugin");
-let extract = require("./extract");
+const plugin = require("tailwindcss/plugin");
+const fs = require("fs");
+const walkdir = require("walkdir");
+const postcss = require("postcss");
+const postcssjs = require("postcss-js");
 
 module.exports = plugin(function ({ addUtilities, addBase }) {
-    let utilities = extract.createUtilities();
-    addUtilities(utilities);
-    console.log("final utilities");
-    console.log(utilities);
-    // let fontFaces = extract.findUsedFontFaces();
-    // let atRules = extract.createFontFaceAtRules(fontFaces);
-    // console.log("final atRules");
-    // console.log(atRules);
-    // addBase(atRules);
+    function className(fontName, fontStyle, fontWeight) {
+        fontName = fontName.replace(/\s/g, "");
+        if (fontStyle === "italic") {
+            fontStyle = "i";
+        }
+        if (fontStyle === "normal") {
+            fontStyle = "";
+        }
+        const clazz = `.font-${fontName}-${fontWeight}`;
+        if (fontStyle === "i") {
+            return `${clazz}-i`;
+        }
+        return clazz;
+    }
+
+    let filez = "";
+
+    function findUsedFontFaces(rootSrcDir = "./src") {
+        const classNameRegex = /font-([^-]+)-(\d+)-?([i])?/g;
+        const files = walkdir.sync(rootSrcDir);
+        const used = [];
+
+        for (const file of files) {
+            filez += file + "\n";
+            try {
+                const fileContent = fs.readFileSync(file, "utf8");
+                filez += fileContent + "\n";
+                let matches;
+                while ((matches = classNameRegex.exec(fileContent)) !== null) {
+                    const [, fontName, fontWeight, fontStyle] = matches;
+                    const className_ = className(
+                        fontName,
+                        fontStyle,
+                        fontWeight
+                    );
+                    if (!used.includes(className_)) {
+                        used.push(className_);
+                    }
+                }
+            } catch (err) {
+                if (err.code === "EISDIR") {
+                    continue;
+                }
+            }
+        }
+
+        return used;
+    }
+
+    function getAtRules(fontFaces, usedFontFaces, addBase) {
+        for (const usedFontFace of usedFontFaces) {
+            const fontFace = fontFaces[usedFontFace];
+            if (fontFace) {
+                addBase(postcssjs.objectify(postcss.parse(fontFace)));
+            }
+        }
+    }
+
+    let utils = JSON.parse(fs.readFileSync("./utils.json", "utf8"));
+    addUtilities(utils);
+    let usedFontFaces = findUsedFontFaces("./src");
+    fs.writeFileSync("./used.json", JSON.stringify(usedFontFaces));
+    let fontFaces = JSON.parse(fs.readFileSync("./fonts.json", "utf8"));
+    getAtRules(fontFaces, usedFontFaces, addBase);
 });
